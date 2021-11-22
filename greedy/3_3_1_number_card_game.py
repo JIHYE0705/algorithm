1b686323482ad322dbcfa4c32adcd00e947db4a3
jihye0705 <jihyem75@gmail.com>
2021-11-22 21:42:39 +0900

    greedy 3-3

---
+++ greedy/3_3_1_number_card_game.py
@@ -0,0 +1,14 @@
+# N,M 입력받기
+n, m = map(int, input().split())
+
+result = 0
+
+# 한 줄씩 입력받아 확인
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 '가장 작은 수' 찾기
+    min_value = min(data)
+    result = max(result, min_value)
+
+print(result)
